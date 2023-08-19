de78fee68167fd28ed43c8a0dd3674c2b77a55a2
Samrat Ahmmed <samratahammed29@gmail.com>
2023-08-19 13:58:36 +0600

    rectangle add and reausable widget create

--- script/area-calculate.js
+++ script/area-calculate.js
@@ -12,5 +12,48 @@ function calculateTriangleArea(){
     const hightValue = hightField.value;
     const hight = parseFloat(hightValue);
 
-    console.log(base , hight);
+  const area = 0.5 * base * hight ;
+  
+
+  const triangleArea = document.getElementById('triangle-area');
+  triangleArea.innerText = area;
+}
+
+function calculateRectangleArea (){
+
+   const widthField = document.getElementById('rectangle-width');
+   const widthValue = widthField.value;
+   const width = parseFloat(widthValue);
+
+
+   const lengthField = document.getElementById('rectangle-length');
+   const lengthValue = lengthField.value;
+   const length = parseFloat(lengthValue);
+
+ const area = width * length;
+
+ const rectangleArea = document.getElementById('rectangle-area');
+ rectangleArea.innerText = area;
+
+}
+
+
+function calculateParallelogramArea(){
+ const base = getInputValue('parallelogram-base');
+ 
+
+ const hight = getInputValue('parallelogram-hight');
+
+ console.log(hight);
+
+}
+
+
+// reusable function 
+function getInputValue(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputValueText = inputField.value;
+    const inputInt = parseFloat(inputValueText);
+    return inputInt;
+
 }
